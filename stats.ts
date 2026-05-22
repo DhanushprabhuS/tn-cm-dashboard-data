4699d535b5cfa236d1b6f10a8a4d26f556983345
Dhanush Prabhu S <Dhanush.S@zentropytech.com>
2026-05-22 07:21:58 +0530

    stats updated

--- stats.ts
+++ stats.ts
@@ -18,5 +18,5 @@ export const statsData: StatCard[] = [
   { id: 20260510001, value: "108", desc: "TVK seats won<br>out of 234 total",        colorClass: "orange" },
   { id: 20260510002, value: "144", desc: "MLA support secured<br>coalition majority", colorClass: "gold"   },
   { id: 20260510003, value: "3",   desc: "Government orders<br>signed on Day 1",      colorClass: "green"  },
-  { id: 20260516001, value: "10",  desc: "Cabinet ministers<br>including CM Vijay",   colorClass: "blue"   },
+  { id: 20260516001, value: "33",  desc: "Cabinet ministers<br>including CM Vijay",   colorClass: "blue"   },
 ];
